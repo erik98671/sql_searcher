663c86b1e95b3588d7e1af573c5c083f4fe1b8c7
Erik Braun <30779061+erik98671@users.noreply.github.com>
2021-06-08 11:21:05 -0700

    Add files via upload

---
+++ SQL_Searcher_PCP360.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# SQL_Searcher_Class.py
+# Last Updated 202103290818
+
+import SQL_Searcher_Class
+
+if __name__ == "__main__":
+    tool = SQL_Searcher_Class.SQL_Searcher()
+
+    index_files = [r"H:\Git\sql_searcher\index_files\sql_file_index_2021.txt",
+                   r"H:\Git\sql_searcher\index_files\sql_file_index_2020.txt",
+                   r"H:\Git\sql_searcher\index_files\sql_file_index_2019.txt",
+                   r"H:\Git\sql_searcher\index_files\sql_file_index_2018.txt",
+                   r"H:\Git\sql_searcher\index_files\sql_file_index_2017.txt",
+                   r"H:\Git\sql_searcher\index_files\sql_file_index_2016.txt"]
+
+    searching_for = "PCP360"
+    tool.search_files(index_files[0], searching_for)
+    tool.search_files(index_files[1], searching_for)
+    tool.search_files(index_files[2], searching_for)
+    tool.search_files(index_files[3], searching_for)
